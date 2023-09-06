05f667478a58d2201e0ddb2339d2ff49a4e28d15
Necati Han <han.necati@gmail.com>
2023-09-06 11:51:25 +0200

    Add UI for rockpaperscissor

--- foundations-course/javascript/project_rock-paper-scissors/script.js
+++ foundations-course/javascript/project_rock-paper-scissors/script.js
@@ -8,30 +8,95 @@ function playRound(player, computer) {
 	player = player.toUpperCase();
 	computer = computer.toUpperCase();
 	if (player === "ROCK" && computer === "SCISSOR") {
+		playerPt++;
+		winnerBackground("#result",1, 0);
 		return ("You Win! Rock beats the Scissor");
 	}
 	else if (player === "ROCK" && computer === "PAPER") {
+		computerPt++;
+		winnerBackground("#result",0, 1);
 		return ("You Lose! Paper beats the Rock");
 	}
 	else if (player === "SCISSOR" && computer === "ROCK") {
+		computerPt++;
+		winnerBackground("#result",0, 1);
 		return ("You Lose! Rock beats the Scissor");
 	}
 	else if (player === "SCISSOR" && computer === "PAPER") {
+		playerPt++;
+		winnerBackground("#result",1, 0);
 		return ("You Win! Scissor beats the Paper");
 	}
 	else if (player === "PAPER" && computer === "ROCK") {
+		playerPt++;
+		winnerBackground("#result",1, 0);
 		return ("You Win! Paper beats the Rock");
 	}
 	else if (player === "PAPER" && computer === "SCISSOR") {
+		computerPt++;
+		winnerBackground("#result",0, 1);
 		return ("You Lose! Scissor beats the Paper");
 	}
 	else {
+		winnerBackground("#result",0, 0);
 		return ("Equality!");
 	}
 }
-const playerSelection = "Rock";
-let computerSelection;
-for (let i = 0; i < 5; i++) {
-	computerSelection = getComputerChoice();
-	console.log(playRound(playerSelection, computerSelection));
+
+function winnerBackground(idName, one, two) {
+	elt = document.querySelector(idName);
+	if (one > two) {
+		elt.classList.remove("equality");
+		elt.classList.remove("computer");
+		elt.classList.add("player");
+	}
+	else if (one < two) {
+		elt.classList.remove("equality");
+		elt.classList.remove("player");
+		elt.classList.add("computer");
+	}
+	else {
+		elt.classList.add("equality");
+		elt.classList.remove("player");
+		elt.classList.remove("computer");
+	}
 }
+
+function handleButtonClick(event) {
+	const playerSelection = event.target.textContent;
+	const computerSelection = getComputerChoice();
+	const divResult = document.querySelector("#result");
+	const olVs = document.querySelector("#vs");
+	const point = document.querySelector("#point");
+	const newLi = document.createElement("li");
+	if (playerPt === 5 || computerPt === 5) {
+		divResult.textContent = "";
+		olVs.replaceChildren();
+		point.textContent = "";
+		playerPt = 0;
+		computerPt = 0;
+		point.classList.remove("equality");
+		point.classList.remove("player");
+		point.classList.remove("computer");
+	}
+	const round = playRound(playerSelection, computerSelection);
+	newLi.textContent = `${playerSelection} vs ${computerSelection}`;
+	olVs.appendChild(newLi);
+	if (playerPt === 5) {
+		divResult.textContent = "You win this fight";
+		winnerBackground("#result",1, 0);
+	}
+	else if (computerPt === 5) {
+		divResult.textContent = "The computer win this fight";
+		winnerBackground("#result",0, 1);
+	}
+	else {
+		divResult.textContent = round;
+	}
+	winnerBackground("#point", playerPt, computerPt);
+	point.textContent = `Player = ${playerPt} Computer = ${computerPt}`;
+}
+let playerPt = 0;
+let computerPt = 0;
+const btnContainer = document.querySelector("#btn-container");
+btnContainer.addEventListener("click", handleButtonClick);
